adc84a583e50f986333ec39d65866e9937ef4f30
Xingjian Chen <chen3xn@vega.seas.upenn.edu>
2023-10-24 19:18:38 -0400

    Finished filter_vertical_HW

--- Filter.cpp
+++ Filter.cpp
@@ -70,7 +70,7 @@ static unsigned Coefficients[] = {2, 15, 62, 98, 62, 15, 2};
 }
 
 
-static void Filter_vertical_SW(const unsigned char * Input,
+ void Filter_vertical_SW(const unsigned char * Input,
 		                    unsigned char * Output)
 {
   int X, Y, i;
@@ -83,6 +83,46 @@ static void Filter_vertical_SW(const unsigned char * Input,
       Output[Y * OUTPUT_FRAME_WIDTH + X] = Sum >> 8;
     }
 }
+
+
+
+void Filter_vertical_HW(const unsigned char * Input,
+		                    unsigned char * Output)
+{
+  int X, Y, i;
+  unsigned char input_buffer[7];
+  for (X = 0; X < OUTPUT_FRAME_WIDTH; X++)
+  {
+	    input_buffer[0]=Input[0*OUTPUT_FRAME_WIDTH+X];
+	  	input_buffer[1]=Input[1*OUTPUT_FRAME_WIDTH+X];
+	  	input_buffer[2]=Input[2*OUTPUT_FRAME_WIDTH+X];
+	  	input_buffer[3]=Input[3*OUTPUT_FRAME_WIDTH+X];
+	  	input_buffer[4]=Input[4*OUTPUT_FRAME_WIDTH+X];
+	  	input_buffer[5]=Input[5*OUTPUT_FRAME_WIDTH+X];
+
+    for (Y = 0; Y < OUTPUT_FRAME_HEIGHT; Y++)
+    {
+      input_buffer[6]=Input[(Y+6)*OUTPUT_FRAME_WIDTH+X];
+      unsigned int Sum = 0;
+
+      for (i = 0; i < FILTER_LENGTH; i++)
+      {
+		#pragma HLS PIPELINE
+		#pragma HLS unroll
+        Sum += Coefficients[i] * input_buffer[i];
+      }
+      Output[Y*OUTPUT_FRAME_WIDTH + X] = Sum >> 8;
+      for(i = 0; i < FILTER_LENGTH-1; i++)
+      {
+    	  input_buffer[i]=input_buffer[i+1];
+      }
+    }
+}
+}
+
+
+
+
 
 void Filter_SW(const unsigned char * Input,
 	           unsigned char * Output)
